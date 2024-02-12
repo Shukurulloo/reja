2b0eaf6518f5cde20070db5c641bd5e821ab9e37
mac <mac@macui-MacBookPro.local>
2024-02-12 12:55:47 +0900

    fix: refactoring

--- train.js
+++ train.js
@@ -3,15 +3,15 @@
 // B-TASK
 
 
-// console.log("Jack Ma maslahatlari");
-// const list = [
-//     "yaxshi talaba bo'ling", // 0-20
-//     "to'gri boshliq tanlang va ko'proq hato qiling", // 20-30
-//     "ozingizga ishlashni boshlang", // 30-40
-//     "siz kuchli bo'lgan narsalarni qiling", // 40-50
-//     "yoshlarga investitsiya qiling", // 50-60
-//     "endi dam oling, foydasi yo'q", // 60
-// ];
+console.log("Jack Ma maslahatlari");
+const list = [
+    "yaxshi talaba bo'ling", // 0-20
+    "to'gri boshliq tanlang va ko'proq hato qiling", // 20-30
+    "ozingizga ishlashni boshlang", // 30-40
+    "siz kuchli bo'lgan narsalarni qiling", // 40-50
+    "yoshlarga investitsiya qiling", // 50-60
+    "endi dam oling, foydasi yo'q", // 60
+];
 
 // // CALLBACK function
 // function maslahatBering (a, callback) {
@@ -43,26 +43,26 @@
 
 
 // ASYNC function
-// async function maslahatBering (a) {
-//     if(typeof a !== "number") throw new Error("insert number");
-//     else if(a <= 20) return list[0];
-//     else if(a > 20 && a <= 30) return list[1];
-//     else if(a > 30 && a <= 40) return list[2];
-//     else if(a > 40 && a <= 50) return list[3];
-//     else if(a > 50 && a <= 60) return list[4];
-//     else {
-//         return new Promise((resolve, reject) => { // resolve => return, reject => throw new Error
-//             setTimeout(() => {                                            
-//                 resolve(list[5]);   
-//             },5000);
-//             // setInterval(() => {                                            
-//             //     resolve(list[5]);   
-//             // },1000);
-//           });
-//        }
-//     }
+async function maslahatBering (a) {
+    if(typeof a !== "number") throw new Error("insert number");
+    else if(a <= 20) return list[0];
+    else if(a > 20 && a <= 30) return list[1];
+    else if(a > 30 && a <= 40) return list[2];
+    else if(a > 40 && a <= 50) return list[3];
+    else if(a > 50 && a <= 60) return list[4];
+    else {
+        return new Promise((resolve, reject) => { // resolve => return, reject => throw new Error
+            setTimeout(() => {                                            
+                resolve(list[5]);   
+            },5000);
+            // setInterval(() => {                                            
+            //     resolve(list[5]);   
+            // },1000);
+          });
+       }
+    }
 
-// // call qilindi then/catch orqali
+// call qilindi then/catch orqali
 // console.log("passed here 0");
 // maslahatBering(20)
 // .then(data => {
@@ -88,30 +88,33 @@
 // console.log("passed here 1");
 
 
-// call qilindi (via) asyn/await orqali
-// async function run() {
-//     let javob = await maslahatBering(25);
-//     console.log(javob);
-//     javob = await maslahatBering(70);
-//     console.log(javob);
-//     javob = await maslahatBering(41);
-//     console.log(javob);
-// }
-// run();
+// call qilindi (via) async/await orqali
+async function run() {
+    let javob = await maslahatBering(25);
+    console.log(javob);
+    javob = await maslahatBering(41);
+    console.log(javob);
+    javob = await maslahatBering(70);
+    console.log(javob);
+    javob = await maslahatBering(52);
+    console.log(javob);
+}
+run();
 
 
 
 // A-TASK
-function countLetter(letter, word) {
-    let count = 0; 
-    for(let i = 0; i < word.length; i++){
-        if (word.charAt(i) === letter){
-            count++; 
-        }
-    }
-    return count; 
-}
+// function countLetter(letter, word) { //
+//     let count = 0; 
+//     for(let i = 0; i < word.length; i++){
+//         if (word.charAt(i) === letter){
+//             count++; 
+//         }
+//     }
+//     return count; 
+// }
 
-console.log(countLetter("u", "Shukurullo")); 
+// console.log("check1:",countLetter("u", "Shukurullo")); 
+// console.log("check2:",countLetter("l", "Shukurullo")); 
 
-
+// richwebdeveloper
