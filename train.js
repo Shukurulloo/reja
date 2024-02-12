c94ccb010b41dd14ce267c76be70c5e8718bec95
mac <mac@macui-MacBookPro.local>
2024-02-12 17:25:27 +0900

    feat: TASK-B

--- train.js
+++ train.js
@@ -3,15 +3,15 @@
 // B-TASK
 
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba bo'ling", // 0-20
-    "to'gri boshliq tanlang va ko'proq hato qiling", // 20-30
-    "ozingizga ishlashni boshlang", // 30-40
-    "siz kuchli bo'lgan narsalarni qiling", // 40-50
-    "yoshlarga investitsiya qiling", // 50-60
-    "endi dam oling, foydasi yo'q", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba bo'ling", // 0-20
+//     "to'gri boshliq tanlang va ko'proq hato qiling", // 20-30
+//     "ozingizga ishlashni boshlang", // 30-40
+//     "siz kuchli bo'lgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yo'q", // 60
+// ];
 
 // // CALLBACK function
 // function maslahatBering (a, callback) {
@@ -43,24 +43,24 @@ const list = [
 
 
 // ASYNC function
-async function maslahatBering (a) {
-    if(typeof a !== "number") throw new Error("insert number");
-    else if(a <= 20) return list[0];
-    else if(a > 20 && a <= 30) return list[1];
-    else if(a > 30 && a <= 40) return list[2];
-    else if(a > 40 && a <= 50) return list[3];
-    else if(a > 50 && a <= 60) return list[4];
-    else {
-        return new Promise((resolve, reject) => { // resolve => return, reject => throw new Error
-            setTimeout(() => {                                            
-                resolve(list[5]);   
-            },5000);
-            // setInterval(() => {                                            
-            //     resolve(list[5]);   
-            // },1000);
-          });
-       }
-    }
+// async function maslahatBering (a) {
+//     if(typeof a !== "number") throw new Error("insert number");
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => { // resolve => return, reject => throw new Error
+//             setTimeout(() => {                                            
+//                 resolve(list[5]);   
+//             },5000);
+//             // setInterval(() => {                                            
+//             //     resolve(list[5]);   
+//             // },1000);
+//           });
+//        }
+//     }
 
 // call qilindi then/catch orqali
 // console.log("passed here 0");
@@ -89,17 +89,17 @@ async function maslahatBering (a) {
 
 
 // call qilindi (via) async/await orqali
-async function run() {
-    let javob = await maslahatBering(25);
-    console.log(javob);
-    javob = await maslahatBering(41);
-    console.log(javob);
-    javob = await maslahatBering(70);
-    console.log(javob);
-    javob = await maslahatBering(52);
-    console.log(javob);
-}
-run();
+// async function run() {
+//     let javob = await maslahatBering(25);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+//     javob = await maslahatBering(70);
+//     console.log(javob);
+//     javob = await maslahatBering(52);
+//     console.log(javob);
+// }
+// run();
 
 
 
@@ -118,3 +118,27 @@ run();
 // console.log("check2:",countLetter("l", "Shukurullo")); 
 
 // richwebdeveloper
+
+
+
+// B-TASK
+
+function raqamniAniqlsh(str) {
+    let count = 0;
+  
+    for (let r of str) {
+      if (r >= "0" && r <= "9") {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  console.log(raqamniAniqlsh("6s5sc 43dfs4dd841v23#"));
+
+
+  // CHALLENG TASKlar
+const animal_list = ["fox", "ant","bird", "lion", "deer", "bear", "frog", "hen", "mole", "duck", "goat", "dog",
+ "cat", "bat", "cock", "cow"];
+
+// function findAnimals(txt)
