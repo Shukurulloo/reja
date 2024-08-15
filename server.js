9d3d39aadec1376377289554785624cc49741c74
mac <mac@macui-MacBookPro.local>
2024-08-16 06:24:21 +0900

    feat: create container via docker-compose.yml and fix: chanched PORT to 3000

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongodb.connect(connectionString, {
 
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = 5009; 
+        let PORT = process.env.PORT || 3000 
         server.listen(PORT, function() { 
             console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
             );
